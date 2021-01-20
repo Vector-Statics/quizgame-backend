c4866613cbcaa777aa3112b3cafec6ef071168d3
Abdul Qoyyuum <abdul.qoyyuum@gmail.com>
2021-01-20 13:55:35 +0000

    Fix Postgresql Version

--- main.tf
+++ main.tf
@@ -17,32 +17,6 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-# resource "google_compute_network" "vpc_network" {
-#   name                    = "terraform-network"
-#   auto_create_subnetworks = "true"
-# }
-
-# resource "google_compute_instance" "default" {
-#   name         = "terraformed-microvm"
-#   machine_type = "f1-micro"
-#   zone         = "us-central1-a"
-
-#   boot_disk {
-#     initialize_params {
-#       image = "debian-cloud/debian-10"
-#       size  = 30
-#       type  = "pd-standard"
-#     }
-#   }
-
-#   network_interface {
-#     network = "default"
-#     access_config {
-
-#     }
-#   }
-# }
-
 resource "google_project" "my_project" {
     name       = "Quiz Game"
     project_id = var.project_id
@@ -56,13 +30,13 @@ resource "google_app_engine_application" "quizgame" {
 
 resource "google_sql_database_instance" "instance"{
     name = "quiz-game-db-instance"
-    database_version = "POSTGRESQL_11"
+    database_version = "POSTGRES_11"
     region = "us-central1"
     settings {
         tier = "db-f1-micro"
     }
 
-    deletion_protection = "true"
+    deletion_protection = "false"
 }
 
 resource "google_sql_database" "database" {
